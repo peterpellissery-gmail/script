55f8dbf743d26582d7ae9fd2b0a830d3dbb7f16a
peterpellissery-gmail <peter.pellissery@gmail.com>
2026-09-24 13:55:32 +0200

    test

--- prod_spp.ps1
+++ prod_spp.ps1
@@ -1,9 +1,9 @@
 # Config
 $thumbprint = "20BDB4CEF549B262AF289855728F66EF5C9997E6"
 $apiKey     = "iw2TNDZgvxeHTqYD4MpxxePlPMV5PWJW1gfE6QZRtWM="
-$sppUrl     = "https://SPP11.IKEA.COM/service/a2a/v4/Credentials?type=Password"
+ #$sppUrl     = "https://SPP11.IKEA.COM/service/a2a/v4/Credentials?type=Password"
 #$sppUrl     = "https://10.59.225.154/service/a2a/v4/Credentials?type=Password"
-#$sppUrl     = "https://a2apam.ikea.com/service/a2a/v4/Credentials?type=Password"
+$sppUrl     = "https://a2apam.ikea.com/service/a2a/v4/Credentials?type=Password"
 #$sppUrl     = "https://10.79.133.217/service/a2a/v4/Credentials?type=Password"
 
 
